6f8e5079bcc830bd2a86792ce928ad7167f8f538
conghieu281 <conghieu8899.work@gmail.com>
2024-11-04 15:41:31 +0700

    add category function

--- src/api/category.ts
+++ src/api/category.ts
@@ -8,6 +8,13 @@ const dashboardCategoryApi = {
         return api.get(`/dashboard/categories/?${query}`)
     },
 
+    createCategory: (data: {
+        title: string,
+        description:string
+    }) => {
+        return api.post('/dashboard/categories', data)
+    }
+
 }
 
 export { dashboardCategoryApi};
